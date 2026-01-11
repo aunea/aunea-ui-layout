a98d4854ab3a16050d38dea71bab41b59acfc44c
GitBella <au.isabella.r.c@hotmail.com>
2026-01-11 09:02:34 -0300

    test(layout): teste visual temporario

--- src/components/assistant/AssistantFrame.tsx
+++ src/components/assistant/AssistantFrame.tsx
@@ -1,8 +1,11 @@
+import "../..//styles/assistant-frame.css"
 export function AssistantFrame() {
     return (
         <div className="assistant-frame">
             <div className="arc-left"></div>
+            <div className="relief relief-left"></div>
             <div className="arc-right"></div>
+            <div className="relief relief-right"></div>
             <div className="mid-circle"></div>
             <div className="inner-circle"></div>
         </div>
